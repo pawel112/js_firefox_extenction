bdfba3dbefd2dc9f27b373a1d117cf79f9c51886
pawel112 <pawel112@users.noreply.github.com>
2017-04-23 10:37:16 +0200

    Działają niektóre opcje

--- js/options.js
+++ js/options.js
@@ -1,19 +1,133 @@
-function saveOptions(e) {
-  browser.storage.local.set({
-    colour: document.querySelector("#colour").value
-  });
-  e.preventDefault();
+function saveOptions(e)
+{
+	//TODO
+	//SPRAWDZANIE PÓL
+	
+	var UpdateFrequencyListValue = null;
+	if (document.getElementsByName("optionsUpdateFrequencyListValue")[0].checked == true)
+	{
+		UpdateFrequencyListValue = 0;
+	}
+	else if (document.getElementsByName("optionsUpdateFrequencyListValue")[1].checked == true)
+	{
+		UpdateFrequencyListValue = 1;
+	}
+	else
+	{
+		UpdateFrequencyListValue = 2;
+	}
+	
+ 	browser.storage.local.set
+ 	({	  
+		optionsUpdateFrequencyListValue: UpdateFrequencyListValue
+	});
+	e.preventDefault();
 }
 
-function restoreOptions() {
-  //var gettingItem = browser.storage.local.get('colour');
-  //gettingItem.then((res) => {
-    //document.querySelector("#colour").value = res.colour || 'Firefox red';
-  //});
+function restoreOptions()
+{
+	browser.storage.local.get().then((res) =>
+	{
+		if (!((res.optionsUpdateFrequencyListValue == 0) || (res.optionsUpdateFrequencyListValue == 1) || (res.optionsUpdateFrequencyListValue == 2)))
+		{
+			res.optionsUpdateFrequencyListValue = 0;
+			browser.storage.local.set
+			({	  
+				optionsUpdateFrequencyListValue: 0
+			});
+		}
+		document.getElementsByName("optionsUpdateFrequencyListValue")[res.optionsUpdateFrequencyListValue].checked = true;
+		
+		if (!((res.optionsLists == "") || (res.optionsLists == null) || (res.optionsLists == undefined)))
+		{
+			//TODO
+			//ZAPIS DO PÓL
+		}
+		else
+		{
+			document.getElementById("optionsLists").innerHTML += "<br/>"+browser.i18n.getMessage("optionsIgnoredNone");	
+			res.optionsLists = "";
+			browser.storage.local.set
+			({	  
+				optionsLists: ""
+			});
+		}
+		
+		if (!((res.optionsIgnoredURL == "") || (res.optionsIgnoredURL == null) || (res.optionsIgnoredURL == undefined)))
+		{
+			//TODO
+			//ZAPIS DO PÓL
+		}
+		else
+		{
+			document.getElementById("optionsIgnoredURL").innerHTML += "<br/>"+browser.i18n.getMessage("optionsIgnoredNone");	
+			res.optionsIgnoredURL = "";
+			browser.storage.local.set
+			({	  
+				optionsIgnoredURL: ""
+			});
+		}
+		
+		if (!((res.optionsIgnoredWord == "") || (res.optionsIgnoredWord == null) || (res.optionsIgnoredWord == undefined)))
+		{
+			//TODO
+			//ZAPIS DO PÓL
+		}
+		else
+		{
+			document.getElementById("optionsIgnoredWord").innerHTML += "<br/>"+browser.i18n.getMessage("optionsIgnoredNone");	
+			res.optionsIgnoredWord = "";
+			browser.storage.local.set
+			({	  
+				optionsIgnoredWord: ""
+			});
+		}
+		
+		if (!((res.optionsIgnoredCourierCompany == "") || (res.optionsIgnoredCourierCompany == null) || (res.optionsIgnoredCourierCompany == undefined)))
+		{
+			//TODO
+			//ZAPIS DO PÓL
+		}
+		else
+		{
+			document.getElementById("optionsIgnoredCourierCompany").innerHTML += "<br/>"+browser.i18n.getMessage("optionsIgnoredNone");	
+			res.optionsIgnoredCourierCompany = "";
+			browser.storage.local.set
+			({	  
+				optionsIgnoredCourierCompany: ""
+			});
+		}
+		
+	});
+}
+
+function addNewFieldList()
+{
+	//TODO
+	//DODAĆ POJAWIANIE SIĘ POLA PUSTEGO
+}
+
+function addNewFieldURL()
+{
+	
+}
+
+function addNewFieldWord()
+{
+	
+}
+
+function addNewFieldCourierCompany()
+{
+	
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
+document.getElementById("optionsAddList").addEventListener("submit", addNewFieldList);
+document.getElementById("optionsAddURL").addEventListener("submit", addNewFieldURL);
+document.getElementById("optionsAddWord").addEventListener("submit", addNewFieldWord);
+document.getElementById("optionsAddCourierCompany").addEventListener("submit", addNewFieldCourierCompany);
 
 document.getElementById("optionsLists").innerHTML += browser.i18n.getMessage("optionsLists");
 document.getElementById("optionsUpdateFrequencyList").innerHTML += browser.i18n.getMessage("optionsUpdateFrequencyList");
@@ -26,3 +140,7 @@ document.getElementById("optionsIgnoredWord").innerHTML += browser.i18n.getMessa
 document.getElementById("optionsIgnoredCourierCompany").innerHTML += browser.i18n.getMessage("optionsIgnoredCourierCompany");
 document.getElementById("optionsTitle").innerHTML = browser.i18n.getMessage("optionsTitle");
 document.getElementById("optionsSave").innerHTML += browser.i18n.getMessage("optionsSave");
+document.getElementById("optionsAddList").innerHTML += browser.i18n.getMessage("optionsAddToList");
+document.getElementById("optionsAddURL").innerHTML = browser.i18n.getMessage("optionsAddToList");
+document.getElementById("optionsAddWord").innerHTML += browser.i18n.getMessage("optionsAddToList");
+document.getElementById("optionsAddCourierCompany").innerHTML += browser.i18n.getMessage("optionsAddToList");
